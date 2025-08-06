8ecd6d9bcd98f9a83e0635191dae2ebbfda7973a
marcustav <marcustpires@gmail.com>
2025-08-06 20:54:52 -0300

    Add files via upload

---
+++ calculadora-imc.js
@@ -0,0 +1,8 @@
+const altura= 1.80;
+const peso = 75;
+
+calcula=peso/(altura*altura);
+console.log("Altura:", altura + "0m")
+console.log("Peso:", peso + "kg")
+console.log("Cálculo do IMC:", calcula)
+
